6131de6b6e2bec5b32948855a74ef2903ef7a6c8
breznovic <slymarkel@gmail.com>
2023-08-17 19:10:28 +0300

    Math rate

--- src/components/MathDraft.tsx
+++ src/components/MathDraft.tsx
@@ -1,14 +1,20 @@
+import { useEffect } from "react";
+
 const calculateMonthlyPayment = () => {
-  /* const monthlyInterestRate = interestRate / 1200;
+  const monthlyInterestRate = interestRate / 1200;
     const numPayments = loanTerm * 12;
     const numerator = monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numPayments);
     const denominator = Math.pow(1 + monthlyInterestRate, numPayments) - 1;
     return loanAmount * (numerator / denominator); */
 };
 
-/* useEffect(() => {
+useEffect(() => {
     const monthlyPayment = calculateMonthlyPayment();
     dispatch(setMonthlyPayment(monthlyPayment));
-  }, [loanAmount, interestRate, loanTerm, dispatch]); */
+  }, [loanAmount, interestRate, loanTerm, dispatch]);
 
+
+function dispatch(arg0: any) {
+  throw new Error("Function not implemented.");
+}
 
